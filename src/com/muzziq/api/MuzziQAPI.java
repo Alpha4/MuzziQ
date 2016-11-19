b5cf557d31bc4600eb38da1443d79f3837900361
AndreiTodor <bvandrei94@gmail.com>
2016-11-19 23:24:52 +0100

    utilisation du memcache, ajout construir questions en fonction du genre

--- src/com/muzziq/api/MuzziQAPI.java
+++ src/com/muzziq/api/MuzziQAPI.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.muzziq.utils.Quizz;
 import com.muzziq.utils.QTemplate;
 import com.muzziq.utils.Question;
@@ -25,6 +33,8 @@ public class MuzziQAPI {
 	private List<QTemplate> templates = new ArrayList<QTemplate>();
 	private List<Question> questions = new ArrayList<Question>();
 	private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	private Logger logger = Logger.getLogger("myLogger");
+	private MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
 	
 	/***
 	 * Dans le constructeur de cette classe on peut rajouter les templates de questions
@@ -34,76 +44,106 @@ public class MuzziQAPI {
 	//TODO completer avec les autres templates
 	
 	public MuzziQAPI(){
-		this.templates.add(new QTemplate("Chanson","Artist","Quel chanteur a composé %%var%%?"));
-		this.templates.add(new QTemplate("Chansons","Artist","Quel chanteur a composé %%var%% et %%var%%?"));
+		this.templates.add(new QTemplate("Single","Artist","Quel chanteur a composé %%var%%?"));
 	}
 	
-	/**
-	 * en fonction de l'information que on donne dans le template de la question, cette fonction
-	 * demande au datastore les veariables necessaires à la creation d'une question puis 
-	 * rajoute la question cree à la liste des questions
-	 * De plus comme les réponses sont dans la classe question ils vont aussi être construit
-	 * @param template
-	 */
 	
-	//TODO finir les autres cas du switch apres avoir rajouté les templates
 	
-	@SuppressWarnings("unchecked")
-	private void createQuestion(QTemplate template){
-		
-		switch(template.getInfoProvided()){
-		case "Chanson":{
-			Random r = new Random();
-			List<Integer> ids = new ArrayList<Integer>();
-			int id = r.nextInt(6);
-			ids.add(id);
-			
-			Filter filter = new FilterPredicate("id", FilterOperator.EQUAL, id);
-			Query q = new Query("QuestionVars").setFilter(filter);
-			PreparedQuery pq = datastore.prepare(q);
-			Entity entity = pq.asSingleEntity();
-			
-			ArrayList<String> songs = (ArrayList<String>) entity.getProperty("Songs");
-			String song = songs.get(r.nextInt(songs.size()));
-			List<String> vars = new ArrayList<String>();
-			vars.add(song);
-			
-			List<String> answers = new ArrayList<String>();
-			String correctAnswer = (String) entity.getProperty("Artist");
-			answers.add(correctAnswer);
-			
-			int i=0;
-			while(i<3){
-				int id1 = r.nextInt(6);
-				if(!ids.contains(id1)){
-					Filter filter1 = new FilterPredicate("id", FilterOperator.EQUAL, id1);
-					Query q1 = new Query("QuestionVars").setFilter(filter1);
-					PreparedQuery pq1 = datastore.prepare(q1);
-					Entity entity1 = pq1.asSingleEntity();
-					String badAnswer = (String) entity1.getProperty("Artist");
-					answers.add(badAnswer);
-					ids.add(id1);
-					i++;
+	private List<Integer> putEntitiesInCache(List<Entity> genreEntities){
+		List<Integer> list = new ArrayList<Integer>();
+		logger.log(Level.INFO, "genreEntities.size() = "+ genreEntities.size());
+		int key = 0;
+		for(int i=0; i< genreEntities.size();i++){
+			Entity e = (Entity) syncCache.get(key);
+			if(e == null){
+				logger.log(Level.INFO,"writing in memcache");
+				e = genreEntities.get(i);
+				syncCache.put(key, e);
+				list.add(key);
+				logger.log(Level.INFO, "syncCache("+i+") = " + syncCache.get(key));
+			}else{
+				if(!e.equals(genreEntities.get(i))){
+					logger.log(Level.INFO,"writing in memcache");
+					e=genreEntities.get(i);
+					syncCache.put(key, e);
+					list.add(key);
+					logger.log(Level.INFO, "syncCache("+i+") = " + syncCache.get(key));
+				}else{
+					list.add(key);
 				}
 			}
-			
-			Question question = new Question(id,template,vars,answers);
-			this.questions.add(question);
-		} break;
-		case "Chansons":{
-			List<String> vars = new ArrayList<String>();
-			List<String> answers = new ArrayList<String>();
-			vars.add("Hero");
-			vars.add("Salvation");
-			answers.add("Skillet");
-			answers.add("Linkin Park");
-			answers.add("Halestorm");
-			answers.add("Nickelback");
-			Question question = new Question(4,template,vars,answers);
-			this.questions.add(question);
-		} break;
+			key++;
 		}
+		return list;
 	}
+	
+	private List<Entity> selectEntitiesByGenre(String genre){
+		Filter filterGenre = new FilterPredicate("Genre", FilterOperator.EQUAL, genre);
+		Query q = new Query("Qvars").setFilter(filterGenre);
+		PreparedQuery pq = this.datastore.prepare(q);
+		Iterable<Entity> itEntity = pq.asIterable();
+		Iterator<Entity> it = itEntity.iterator();
+		List<Entity> elist = new ArrayList<Entity>();
+		while(it.hasNext()){
+			Entity e = it.next();
+			logger.log(Level.INFO, "add()");
+			elist.add(e);
+		}
+		
+		return elist;
+	}
+	
+	private void createQuestion2(QTemplate template,String genre,List<Integer> keys){
+		List<Integer> ids = new ArrayList<Integer>(); //id dans datastore
+		List<Integer> localids = new ArrayList<Integer>();//id dans memcache
+		List<String> answers = new ArrayList<String>();
+		List<String> vars = new ArrayList<String>();
+		
+		String varContext = template.getInfoProvided();
+		String answerContext = template.getInfoDemanded();
+		Random r = new Random();
+		
+		int size = syncCache.getAll(keys).size();
+		logger.log(Level.INFO,"memcache.size() = " + size);
+		int id = r.nextInt(size);
+		Entity correctEntity = (Entity) this.syncCache.get(id);
+		String var = (String) correctEntity.getProperty(varContext);
+		logger.log(Level.INFO, "var =  "+ var);
+		
+		String answer = (String) correctEntity.getProperty(answerContext);
+		logger.log(Level.INFO,"answer = "+ answer);
+		
+		int correctId = (int) correctEntity.getKey().getId();
+		logger.log(Level.INFO,"id = "+ correctId);
+		
+		ids.add(correctId);
+		localids.add(id);
+		vars.add(var);
+		answers.add(answer);
+		
+		int i = 0;
+		while(i<3){
+			int bid = r.nextInt(size);
+			if(localids.contains(bid)){
+				continue;
+			}else{
+				Entity bentity = (Entity) this.syncCache.get(bid);
+				String banswer = (String) bentity.getProperty(answerContext);
+				int badid = (int) bentity.getKey().getId();
+				
+				ids.add(badid);
+				localids.add(bid);
+				answers.add(banswer);
+				
+				i++;
+			}
+		}
+		
+		Question question = new Question(ids.get(0),template,vars,answers);
+		logger.log(Level.INFO, "Question = " + question.getContent());
+		this.questions.add(question);
+	}
+	
 	
 	/**
 	 * Methode de l'api qui va être executé à chaque requete GET sur l'URL de l'API /quizz  
@@ -113,10 +153,13 @@ public class MuzziQAPI {
 	
 	@ApiMethod(name="getQuizz")
 	public Quizz getQuizz(){
+		Quizz myQuizz = new Quizz(1,"Rock");
+		
+		List<Integer> listKeys = this.putEntitiesInCache(this.selectEntitiesByGenre(myQuizz.getGenre()));
 		this.questions.clear();
-		Quizz myQuizz = new Quizz(1);
+		
 		for(int i=0;i<this.templates.size();i++){
-			this.createQuestion(this.templates.get(i));
+			this.createQuestion2(this.templates.get(i),myQuizz.getGenre(), listKeys);
 			myQuizz.addQuestion(this.questions.get(i));
 		}
 		return myQuizz;
