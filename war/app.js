cee38c3c13f9f97aa31f476c40176a2460ccc54c
AndreiTodor <bvandrei94@gmail.com>
2016-11-24 22:18:27 +0100

    interface affichage des questions sur un genre de musique

--- war/app.js
+++ war/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app',['ngRoute','ngStorage']).config(['$routeProvider','$locationProvider',
+var app = angular.module('app',['ngRoute']).config(['$routeProvider','$locationProvider',
     function($routeProvider,$locationProvider){
       $routeProvider
         .when('/main',{
@@ -19,27 +19,31 @@ function init(){
 }
 
 
+//controlleur parent qui possede les methodes pour appeler l'api et des variables de $scope qui se propagent aux autres controlleurs fils
 app.controller('mainControler',['$scope','$route','$location','$window',function($scope,$route,$location){
+  
+  //à l'acces au site(ex : muzziq.appspot.com ), angular va appeler la premiere vue( celle qui a menuCtrl comme controlleur)
   console.log($location.url());
   if($location.url() == ""){
 	  $location.path("/main");
   }
   this.$route = $route;
   
-  console.log("app controller");
+  //definition des variables pouvant etre appellés dans les nested controllers(menuCtrl,gameCtrl)
+  $scope.questions = null;
+  $scope.ind = 0;
   
-  
+  //fonction appelant gapi.client.muzziqapi pour demander le quizz
   $scope.getQuizz = function(genre){
 	  console.log("getquizz method");
-	  var req = gapi.client.muzziqapi.getQuizz({"Genre" : genre});
-	  var resp = req.execute(function(response){
-		  console.log(response);
+	  var req = gapi.client.muzziqapi.getQuizz({"Genre" : genre}).execute(function(response){
 		  $scope.questions = response.questions;
+		  console.log("created scope.questions");
+		  $scope.$apply();
 	  });
-	  console.log($scope.questions);
   };
   
-  
+  //initialisation de gapi
   window.init = function(){
 	  console.log("calling window.init()")
 	  var rootApi = "https://coral-147014.appspot.com/_ah/api/";
@@ -50,15 +54,13 @@ app.controller('mainControler',['$scope','$route','$location','$window',function
 }]);
 
 
-
+//controlleur pour la page d'acqueil (homepage)
 app.controller('menuCtrl',['$scope','$location','$route',function($scope,$location,$route){
   this.$route = $route;
   this.player = player;
-  this.test = 'aplicatia merge bine!';
-  console.log('functioneaza!');
-  this.genre = "Rock";
+  this.genres = genres;
+  console.log(this.genres)
   this.play = function(){
-    //$localStorage.player = this.player;
     console.log("play() invoked");
     $location.path("/ongame");
   };
@@ -66,18 +68,52 @@ app.controller('menuCtrl',['$scope','$location','$route',function($scope,$locati
 
 
 
-app.controller('gameCtrl',['$scope','$route',function($scope,$route){
+//controlleur pour le deroulement du quizz liée à /onquizz
+app.controller('gameCtrl',['$scope','$route','$timeout',function($scope,$route,$timeout){
   this.$route = $route;
-  console.log("game controller");
   this.player = player;
   console.log(this.player);
-  this.ind = 0;
+  this.ind = $scope.ind;
   
-  $scope.getQuizz("Rock");
+  
+  //fonction qui modifie le modele en passant à la question suivante
+  //TODO a finir 
+  this.onclick = function(){
+	  if(this.ind < $scope.questions.length - 1){
+		  console.log("inside onclick() method");
+		  this.ind++;
+		  this.question = $scope.questions[this.ind].content;
+		  this.answers= $scope.questions[this.ind].answers;
+		  console.log(this.question);
+		  console.log(this.ind);
+	  }
+  }
+  
+  //appel à la methode du api qui retourne le quizz
+  $scope.getQuizz(this.player.quizzGenre);
+  
+  //verification que le quizz est bien retourné avant de l'aficher
+  //laisser "me" car si on utilise this dans $scope.$watch(...) ceci ne correspond pas au controlleur 
+  var me = this;
+  $scope.$watch("questions",function(newVal,oldVal){
+	  if(newVal !== oldVal){
+		  console.log("newVal = "+newVal);
+		  console.log("oldVal = "+oldVal);
+		  console.log("$scope.questions = "+$scope.questions);
+		  console.log("ind = ", me.ind);
+		  console.log("me version");
+		  me.question = $scope.questions[me.ind].content;
+		  me.answers = $scope.questions[me.ind].answers;
+	  }
+  });
   
 }]);
 
 var player={
   name:'',
-  score:0
-}
+  score:0,
+  quizzGenre:''
+};
+
+//TODO ajouter les autres genres 
+var genres = ["Rock","Instrumental"];
