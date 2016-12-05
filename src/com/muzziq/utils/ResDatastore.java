0ce056e82816bdf664b7a8f26ac4ffd4fa85cfaf
AndreiTodor <bvandrei94@gmail.com>
2016-12-05 23:18:28 +0100

    java bean pour le retour de la methode fillDataStore

---
+++ src/com/muzziq/utils/ResDatastore.java
@@ -0,0 +1,15 @@
+package com.muzziq.utils;
+
+public class ResDatastore {
+	private boolean isOk;
+	public ResDatastore(boolean isOk){
+		this.setOk(isOk);
+	}
+	public boolean isOk() {
+		return isOk;
+	}
+	public void setOk(boolean isOk) {
+		this.isOk = isOk;
+	}
+	
+}
