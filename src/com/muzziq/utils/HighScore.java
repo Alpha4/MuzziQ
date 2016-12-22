18241680907bffe3960beeba53dbce21669ebf0a
AndreiTodor <bvandrei94@gmail.com>
2016-12-23 00:18:24 +0100

    supprimé champs inutiles

--- src/com/muzziq/utils/HighScore.java
+++ src/com/muzziq/utils/HighScore.java
@@ -4,50 +4,36 @@ import java.lang.String;
 
 public class HighScore{
 
-	private String _googleID;
 	private String _nom;
-	private String _prenom;
-	private int _score;
+	private long _score;
 
-	public HighScore(String gid, String name, String fname, int score){
-		_googleID = gid;
+	public HighScore(String name, long score){
 		_nom = name;
-		_prenom = fname;
 		_score = score;
 	
 	}
 
 	//getters
-	public String getGoogleID(){
-		return _googleID;
-	}
+	
 	
 	public String getNom(){
 		return _nom;
 	}
 	
-	public String getPrenom(){
-		return _prenom;
-	}
 	
-	public int getScore() {
+	public long getScore() {
 		return _score;
 	}
 	
 	//setters
-	public void setGoogleID(String googleID){
-		_googleID = googleID;
-	}
+	
 	
 	public void setNom( String name){
 		_nom = name;
 	}
 	
-	public void setPrenom( String fname){
-		_prenom = fname;
-	}
 	
-	public void setScore( int score) {
+	public void setScore( long score) {
 		_score = score;
 	}
 	
